fc717a77493fd01b81704ab1856a8cd04493a007
Dary <dady281100@gmail.com>
2022-11-06 22:35:11 -0600

    DataTypes solved

--- Java/Datatypes.java
+++ Java/Datatypes.java
@@ -30,20 +30,27 @@ n can't be fitted anywhere.
 
 package Java;
 import java.util.*;
+import java.io.*;
 public class Datatypes {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t=sc.nextInt();
-
+        
+        // First solution
         for(int i=0;i<t;i++)
         {
-
+            // Introducing manually values of byte, short, int and long
             try
             {
                 long x=sc.nextLong();
                 System.out.println(x+" can be fitted in:");
                 if(x>=-128 && x<=127)System.out.println("* byte");
                 //Complete the code
+                if(x>=-32768 && x<=32768)System.out.println("* short");
+                if(x>=-2147483648 && x<=2147483647)System.out.println("* int");
+                if(x>=-9223372036854775808L && x<=9223372036854775807L)System.out.println("* long");
+                
+                
             }
             catch(Exception e)
             {
