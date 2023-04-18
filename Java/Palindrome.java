0ff8f9c6d96e5eec7c011e18576b8de3a7bd8ea1
Israel Aguilar <dady281100@gmail.com>
2023-04-17 23:32:55 -0500

    Palindrome assignment completed

---
+++ Java/Palindrome.java
@@ -0,0 +1,49 @@
+// A palindrome is a word, phrase, number, or other sequence of characters which reads the same backward or forward.
+
+// Given a string A, print Yes if it is a palindrome, print No otherwise.
+
+// Constraints
+
+// A will consist at most 50 lower case english letters.
+// Sample Input
+
+// madam
+// Sample Output
+
+// Yes
+
+
+
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+        int length = A.length();
+        boolean isPal = true;
+        
+        for(int beginIndex = 0; beginIndex < length; beginIndex++)
+        {
+          if(A.charAt(beginIndex) != A.charAt(length-1-beginIndex)) {
+            System.out.println("No");
+            isPal = false;
+            break;
+          }
+        }
+          
+        if(isPal) {
+          System.out.println("Yes");
+        }
+        
+        
+    }
+}
+
+
+
